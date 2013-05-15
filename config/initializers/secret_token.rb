17e29622e55071da4200e69888461dbaacea2c01
magnusolafur <magnusm12@ru.is>
2013-05-15 17:02:04 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Earthquakes::Application.config.secret_token = '1ac6d6fd48335aa884e779f3e67584e8dc5fa47d3265d7b6c4993dc7d81e234a0ded0ad174dae5d3af80da15d1eddb1c46acb099fb1743f42443d104c8dfa788'
